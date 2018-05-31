88db77dffb5834a534fd0102a7ab6a5bc1f7a5b7
denchik30 <mr.deonis@gmail.com>
2018-05-31 19:35:55 +0300

    echo-bot

--- opbot.py
+++ opbot.py
@@ -4,8 +4,8 @@ import telebot
 TOKEN = '527221399:AAGbt7upt7Y82EyXwPgZiyP1xZYkjQVkKr0'
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    sent = bot.send_message(message.chat.id, 'Привет! Как дела?')
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
 
 bot.polling()
