90b058c482c1677ab8cb42ea8b2264f0cd19a92d
denchik30 <mr.deonis@gmail.com>
2018-05-31 19:29:23 +0300

    Update opbot.py

--- opbot.py
+++ opbot.py
@@ -6,6 +6,6 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    sent = bot.sent_message(message.chat.id, 'Привет! Как дела?')
+    sent = bot.send_message(message.chat.id, 'Привет! Как дела?')
 
 bot.polling()
